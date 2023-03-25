d6d78265d87609f1107b75401c449bf66e1579f7
Adeshen <878600556@qq.com>
2023-03-25 13:12:00 +0800

    add process bar

---
+++ echo/wait.sh
@@ -0,0 +1,17 @@
+function waiting()
+{
+    i=0
+    while [ $i -le 100 ]
+    do
+        for j in '\\' '|' '/' '-'
+        do
+            printf "\t\t\t\t%c%c%c%c%c test waiting %c%c%c%c%c\r" \
+            "$j" "$j" "$j" "$j" "$j" "$j" "$j" "$j" "$j" "$j"
+            sleep 0.1
+        done
+        let i=i+4
+    done
+}
+
+waiting
+echo "123"
